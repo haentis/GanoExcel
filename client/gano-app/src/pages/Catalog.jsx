09570c6d87a38c3052bb4bf4bebef196da4bbff6
Iskander <135708220+haentis@users.noreply.github.com>
2025-01-04 00:58:19 +0500

    цц

--- client/gano-app/src/pages/Catalog.jsx
+++ client/gano-app/src/pages/Catalog.jsx
@@ -1,13 +1,16 @@
-import MainCatalog from "../components/Catalog/MainCatalog";
+// import MainCatalog from "../components/Catalog/MainCatalog";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+// import { lazy} from 'react';
+
+// const MarkdownPreview = lazy(() => import('../components/Catalog/MainCatalog'));
 
 
 function Catalog(){
     return(
         <>
           <Header/>
-          <MainCatalog/>
+          <MarkdownPreview/>
           <Footer/>
         </>
     )
